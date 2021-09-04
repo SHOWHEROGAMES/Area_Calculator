5465a9a87c9c8418def588dd2fe2cb03e41156c6
SHOWHEROGAMES <idatenshow758@gmail.com>
2021-09-04 22:13:21 +0530

    sus

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-function calculate(){
-    document.getElementById("textbox").value
+function calculate(){ var area =
+    Number(document.getElementById("textbox").value*Number(document.getElementById("textbox").value));
+    document.getElementById("area").innerHTML=area;}
+function calculate2(){
+    var perimeter = 
+    Number(document.getElementById("textbox").value)*4;
+    document.getElementById("perimeter").innerHTML=perimeter;
+    
   }
